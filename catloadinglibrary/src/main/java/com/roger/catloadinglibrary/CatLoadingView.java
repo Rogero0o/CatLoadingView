457d9e7af73234580206cdaa06cfa8abdc8245b5
Rogero0o <402435491@qq.com>
2016-04-01 17:04:42 +0800

    Initial commit

---
+++ catloadinglibrary/src/main/java/com/roger/catloadinglibrary/CatLoadingView.java
@@ -0,0 +1,117 @@
+package com.roger.catloadinglibrary;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by Administrator on 2016/3/30.
+ */
+public class CatLoadingView extends DialogFragment {
+
+    public CatLoadingView() {
+    }
+
+
+    Animation operatingAnim, eye_left_Anim, eye_right_Anim;
+
+    Dialog mDialog;
+
+    View mouse, eye_left, eye_right;
+
+    EyelidView eyelid_left, eyelid_right;
+
+    GraduallyTextView mGraduallyTextView;
+
+
+    @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
+        if (mDialog == null) {
+            mDialog = new Dialog(getActivity(), R.style.cart_dialog);
+            mDialog.setContentView(R.layout.catloading_main);
+            mDialog.setCanceledOnTouchOutside(true);
+            mDialog.getWindow().setGravity(Gravity.CENTER);
+
+            operatingAnim = AnimationUtils.loadAnimation(getContext(),
+                    R.anim.animition);
+            eye_left_Anim = AnimationUtils.loadAnimation(getContext(),
+                    R.anim.animition);
+            eye_right_Anim = AnimationUtils.loadAnimation(getContext(),
+                    R.anim.animition);
+
+            LinearInterpolator lin = new LinearInterpolator();
+            operatingAnim.setInterpolator(lin);
+            eye_left_Anim.setInterpolator(lin);
+            eye_right_Anim.setInterpolator(lin);
+
+            View view = mDialog.getWindow().getDecorView();
+
+            mouse = view.findViewById(R.id.mouse);
+
+            eye_left = view.findViewById(R.id.eye_left);
+
+            eye_right = view.findViewById(R.id.eye_right);
+
+            eyelid_left = (EyelidView) view.findViewById(R.id.eyelid_left);
+
+            eyelid_left.setColor(Color.parseColor("#d0ced1"));
+
+            eyelid_left.setFromFull(true);
+
+            eyelid_right = (EyelidView) view.findViewById(R.id.eyelid_right);
+
+            eyelid_right.setColor(Color.parseColor("#d0ced1"));
+
+            eyelid_right.setFromFull(true);
+
+            mGraduallyTextView = (GraduallyTextView) view.findViewById(
+                    R.id.graduallyTextView);
+        }
+        return mDialog;
+    }
+
+
+    @Override public void onResume() {
+        super.onResume();
+        mouse.setAnimation(operatingAnim);
+        eye_left.setAnimation(eye_left_Anim);
+        eye_right.setAnimation(eye_right_Anim);
+        eyelid_left.startLoading();
+        eyelid_right.startLoading();
+        mGraduallyTextView.startLoading();
+    }
+
+
+    @Override public void onPause() {
+        super.onPause();
+
+        operatingAnim.cancel();
+        eye_left_Anim.cancel();
+        eye_right_Anim.cancel();
+
+        operatingAnim.reset();
+        eye_left_Anim.reset();
+        eye_right_Anim.reset();
+
+        mouse.clearAnimation();
+        eye_left.clearAnimation();
+        eye_right.clearAnimation();
+
+        eyelid_left.stopLoading();
+        eyelid_right.stopLoading();
+        mGraduallyTextView.stopLoading();
+    }
+
+
+    @Override public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        mDialog = null;
+        System.gc();
+    }
+}
