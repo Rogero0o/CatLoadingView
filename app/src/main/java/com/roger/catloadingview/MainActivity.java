17f8917a7b63767e93af30c2dedba54a0cd3a0d1
Roger Luo <roger.luo@ringcentral.com>
2018-03-19 17:01:29 +0800

    add setCanceledOnTouchOutside method

--- app/src/main/java/com/roger/catloadingview/MainActivity.java
+++ app/src/main/java/com/roger/catloadingview/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(
                 new View.OnClickListener() {
                     @Override public void onClick(View v) {
-                        mView.show(getSupportFragmentManager(), "");
+                        showDialog();
                     }
                 });
     }
